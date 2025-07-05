f76ab1ad15765ff2ca38c47ecf009383e5b1b97e
Alexandr <sanchesterus6@gmail.com>
2025-07-05 12:14:15 +0300

    init .toml

--- app/main.py
+++ app/main.py
@@ -1,16 +1,43 @@
-
+from http.client import HTTPException
 from typing import Optional
 
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 app = FastAPI()
 
 class Task(BaseModel):
     name: str
 
-@app.get("/tasks")
-def get_tasks():
-    task = Task(name="дай мне новую задачу!")
-    return {"data": task}
+books = [
+    {
+        "id": 1,
+        "name": "Адольф",
+        "description": "супер интересная книга"
+    },
+    {
+        "id": 2,
+        "name": "Принц на белом коне",
+        "description": "ваще крутой конь на белом коне"
+    }
+]
 
+@app.get("/books",
+         tags=["Книги"],
+         summary="Получить все книги")
+def get_books():
+    return books
+
+@app.get("/books/{book_id}",
+         tags=["Книги"],
+         summary="Получить одну книгу"
+         )
+def get_book_id(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Книга не найдена")
+
+
+#@app.post("/books")
 #app.include_router(get_tasks)
