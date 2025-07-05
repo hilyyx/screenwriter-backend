8307324c63ac9c62be8ca0c6aba4e02583dafc43
Alexandr <sanchesterus6@gmail.com>
2025-07-05 09:31:41 +0300

    init app

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-print("hello")
+
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
+
+class Task(BaseModel):
+    name: str
+
+@app.get("/tasks")
+def get_tasks():
+    task = Task(name="дай мне новую задачу!")
+    return {"data": task}
+
+#app.include_router(get_tasks)
