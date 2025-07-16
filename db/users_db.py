290d7c0b958a0429840efc3ea92b22adf4282008
Alexandr <sanchesterus6@gmail.com>
2025-07-16 15:03:38 +0300

    future database.py and users_db complete

--- db/users_db.py
+++ db/users_db.py
@@ -6,43 +6,74 @@ class Users:
     def __init__(self, db: Database):
         self.db = db
 
-    def create_user(self, mail, name, surname, password_hash):
+    def create_user(self, mail: str, name: str, surname: str, password_hash: str):
         try:
             self.db.cursor.execute(
-                """INSERT INTO users (mail, name, surname, password_hash)
-                   VALUES (%s, %s, %s, %s) RETURNING id;""",
+                """
+                INSERT INTO users (mail, name, surname, password_hash)
+                VALUES (%s, %s, %s, %s)
+                RETURNING id;
+                """,
                 (mail, name, surname, password_hash)
             )
-            user_id = self.db.cursor.fetchone()['id']
+            user_id = self.db.cursor.fetchone()["id"]
             self.db.conn.commit()
             logger.info(f"The user has been created: {user_id} ({name})")
             return user_id
         except Exception as e:
-            logger.error(f"Error when creating a user {name}: {e}")
+            logger.error(f"Error when creating user {name}: {e}")
+            self.db.conn.rollback()
 
-    def get_user_by_mail(self, mail):
+    def get_user_by_mail(self, mail: str):
         try:
-            self.db.cursor.execute("SELECT * FROM users WHERE mail = %s;", mail)
+            self.db.cursor.execute("SELECT * FROM users WHERE mail = %s;", (mail,))
             user = self.db.cursor.fetchone()
-            logger.info(f"Received a user with an email: {mail}")
+            logger.info(f"Received user by mail: {mail}")
             return user
         except Exception as e:
-            logger.error(f"Error when receiving the user {mail}: {e}")
+            logger.error(f"Error when receiving user by mail {mail}: {e}")
 
-    def update_user_name(self, user_id, new_name):
+    def get_user_by_id(self, user_id: int):
+        try:
+            self.db.cursor.execute("SELECT * FROM users WHERE id = %s;", (user_id,))
+            user = self.db.cursor.fetchone()
+            logger.info(f"Received user by id: {user_id}")
+            return user
+        except Exception as e:
+            logger.error(f"Error when receiving user by id {user_id}: {e}")
+
+    def update_user_name(self, user_id: int, new_name: str, new_surname: str):
         try:
             self.db.cursor.execute(
-                "UPDATE users SET name = %s WHERE id = %s;", (new_name, user_id)
+                "UPDATE users SET name = %s, surname = %s WHERE id = %s;",
+                (new_name, new_surname, user_id)
             )
             self.db.conn.commit()
-            logger.info(f"User name {user_id} updated to {new_name}")
+            logger.info(f"User name {user_id} updated ")
+            return True
         except Exception as e:
-            logger.error(f"Error updating the username {user_id}: {e}")
+            logger.error(f"Error updating user name {user_id}: {e}")
+            self.db.conn.rollback()
 
-    def delete_user(self, user_id):
+    def update_user_password(self, user_id: int, new_pass: str):
+        try:
+            self.db.cursor.execute(
+                "UPDATE users SET password_hash = %s WHERE id = %s;",
+                (new_pass, user_id)
+            )
+            self.db.conn.commit()
+            logger.info(f"Password updated for user {user_id}")
+            return True
+        except Exception as e:
+            logger.error(f"Error updating password for user {user_id}: {e}")
+            self.db.conn.rollback()
+
+    def delete_user(self, user_id: int):
         try:
             self.db.cursor.execute("DELETE FROM users WHERE id = %s;", (user_id,))
             self.db.conn.commit()
-            logger.info(f"The user has been deleted: {user_id}")
+            logger.info(f"User {user_id} deleted")
+            return True
         except Exception as e:
-            logger.error(f"Error when deleting a user {user_id}: {e}")
+            logger.error(f"Error deleting user {user_id}: {e}")
+            self.db.conn.rollback()
